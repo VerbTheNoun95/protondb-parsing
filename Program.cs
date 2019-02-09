574621a24bae01c4f0e949e774d7b15f40b616fc
adbartni <adbartni@bufflo.edu>
2019-02-09 15:09:09 -0500

    Starting to mess with network creds

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace ProtonDB_Parsing
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string libraryUrl = "https://steamcommunity.com/id/" + username + "/games/?tab=all";
+            string loginId = Console.ReadLine();
+            string password = Console.ReadLine();
+            
+            HtmlWeb web = new HtmlWeb();
+            var webClient = new WebClient();
+            webClient.Credentials = new NetworkCredential(loginId, password);
+
+            WebRequest wr = WebRequest.Create(libraryUrl);
+            wr.Credentials = webClient.Credentials;
+
+        }
+    }
+}

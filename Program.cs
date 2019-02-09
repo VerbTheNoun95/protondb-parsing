f333bb734fb2de009dd734669378c5d93ec466ca
VerbTheNoun95 <abartnik@hotmail.com>
2019-02-09 16:42:37 -0500

    Trying to figure out how Steam organizes its library page

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using HtmlAgilityPack;
 
@@ -9,43 +10,29 @@ namespace ProtonDB_Parsing
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("What is your Steam username?");
-            string username = Console.ReadLine();
-            string libraryUrl = "https://steamcommunity.com/id/" + username + "/games/?tab=all";
-            
-            Console.WriteLine("Enter your SteamID");
-            string loginId = Console.ReadLine();
-            Console.WriteLine("Enter your password:");
-            string password = Console.ReadLine();
-            
+            // Scratch work to eventually factor into sane compartments
+            Console.WriteLine("Please enter your Steam username:");
+            string personaName = Console.ReadLine();
+            string libraryUrl = "https://steamcommunity.com/id/" + personaName + "/games/?tab=all";
+
             HtmlWeb web = new HtmlWeb();
-            var webClient = new WebClient();
-            webClient.Credentials = new NetworkCredential(loginId, password);
+            HtmlDocument doc = web.Load(libraryUrl);
 
-            WebRequest request = WebRequest.Create(libraryUrl);
-            request.Credentials = webClient.Credentials;
-            if (request.ContentLength != 0)
+            var entry = doc.DocumentNode.SelectNodes("/html/body/div//div//div//div//div//div//div//div//div//div//div//a");
+
+            if (entry != null)
             {
-                Console.Write("Yeet\n");
-
+                foreach (var node in entry)
+                {
+                    Console.WriteLine(node.InnerText);
+                }
             }
-            
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
-            // Display the status.
-            Console.WriteLine (response.StatusDescription);
-            // Get the stream containing content returned by the server.
-            Stream dataStream = response.GetResponseStream ();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader (dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd ();
-            // Display the content.
-            Console.WriteLine (responseFromServer);
-            // Cleanup the streams and the response.
-            reader.Close ();
-            dataStream.Close ();
-            response.Close ();
+            else
+            {
+                Console.WriteLine("Node empty.");
+            }
 
+            Console.ReadKey(true);
         }
     }
 }

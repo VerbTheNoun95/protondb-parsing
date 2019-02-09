406fb8235e4970ed299507978ef540b931d14c86
adbartni <adbartni@buffalo.edu>
2019-02-09 11:41:51 -0500

    Started project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace ProtonDB_Parsing
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
